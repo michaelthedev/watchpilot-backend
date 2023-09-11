8a58e6443db3c615e85adefd425625d4ac387cd1
Michael A <39175160+michaelthedev@users.noreply.github.com>
2023-09-11 21:28:18 +0100

    App config

---
+++ config/app.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'name' => 'WatchPilot',
+    'url' => 'https://www.watchpilot.test',
+    'timezone' => 'Africa/Lagos',
+    'apiProvider' => 'tmdb',
+    'providers' => [
+        'tmdb' => [
+            'apiKey' => 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhY2U4YmFlMDIzOGIxMGU4NjlkNGU1YjU1ODU3MTI4NyIsInN1YiI6IjY0ZmY0ZGU0MGJiMDc2MDBlMWI4YjZlOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9QOCPLxPqioe0p9_YwEtcehqLWLHKJJ9HY5Y1UNFLN4',
+        ]
+    ]
+];
