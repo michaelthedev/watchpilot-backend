07a176b253f2b5461e0712a5d72dd3757cddfbdd
Michael A <39175160+michaelthedev@users.noreply.github.com>
2023-09-11 21:27:11 +0100

    ApiController

---
+++ app/Controllers/ApiController.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Controllers;
+
+use App\Services\MediaService;
+
+/**
+ * API Controller
+ *
+ * Handles requests for /api/* routes
+ * @package App\Controllers
+ * @author Michael Arawole <michael@logad.net>
+ */
+final class ApiController
+{
+    public function index(): void
+    {
+        $appName = config('app.name');
+        response()->json([
+            'error' => false,
+            'message' => 'Welcome to '.$appName.' API'
+        ]);
+    }
+
+    public function featuredAndTrending(): void
+    {
+        response()->json([
+            'error' => false,
+            'message' => 'Trending and Featured Movies',
+            'data' => [
+                'featured' => [],
+                'trending' => MediaService::getTrendingMoviesAndShows(),
+            ]
+        ]);
+    }
+}
