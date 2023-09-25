ff589738369378f67060c16fb1d091953658eea2
Michael A <39175160+michaelthedev@users.noreply.github.com>
2023-09-25 22:34:54 +0100

    chore: Added `getTvDetails` abstract method

--- app/Interfaces/ApiProviderInterface.php
+++ app/Interfaces/ApiProviderInterface.php
@@ -3,10 +3,13 @@
 namespace App\Interfaces;
 
 use App\DTO\MovieDetail;
+use App\DTO\TvDetail;
 
 interface ApiProviderInterface
 {
     public function getTrendingMoviesAndShows(): array;
 
     public function getMovieDetails(int $id): MovieDetail;
+
+    public function getTvDetails(int $id): TvDetail;
 }
