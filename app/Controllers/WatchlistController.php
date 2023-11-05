f6ca96d2b1cfd97992c8ca8f5b0ff99b2fd3cdb1
Michael A <39175160+michaelthedev@users.noreply.github.com>
2023-11-05 22:48:10 +0100

    Add watchlist model and controller

---
+++ app/Controllers/WatchlistController.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+/**
+ * Watchlist Controller
+ *
+ * @package App\Controllers
+ * @author Michael Arawole <michael@logad.net>
+ */
+final class WatchlistController
+{
+    public function index(): void
+    {
+        $user = request()->user;
+
+        response()->json([
+            'error' => false,
+            'message' => 'success',
+            'data' => $user->watchlists
+        ]);
+    }
+
+    public function store(): void
+    {
+        validate([
+            'name' => 'required',
+        ]);
+
+        $user = request()->user;
+        $name = input('name');
+
+        $watchlist = $user->watchlists()->create([
+            'name' => $name
+        ]);
+
+        response()->json([
+            'error' => false,
+            'message' => 'Watchlist created successfully',
+            'data' => $watchlist
+        ]);
+    }
+}
---
+++ app/Models/Watchlist.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+/**
+ * Watchlist Model
+ *
+ * @package App\Models
+ * @author Michael Arawole <michael@logad.net>
+ */
+final class Watchlist extends Model
+{
+    protected $fillable = ['name'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function items(): HasMany
+    {
+        return $this->hasMany(WatchlistItem::class);
+    }
+}
