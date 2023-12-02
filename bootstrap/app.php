21483895c8acd32affb5d5216b6a59b8b0bbbe7e
Michael A <39175160+michaelthedev@users.noreply.github.com>
2023-12-02 14:31:02 +0100

    Add .env support

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,6 +8,12 @@ define("BASE_PATH", dirname(__FILE__, 2));
 // Include the composer autoloader
 require BASE_PATH.'/vendor/autoload.php';
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../');
+$envFile = BASE_PATH . '/.env';
+if (file_exists($envFile)) {
+	$dotenv->load();
+}
+
 // Misc
 require __DIR__.'/constants.php';
 require __DIR__.'/database.php';
--- config/app.php
+++ config/app.php
@@ -7,7 +7,7 @@ return [
     'apiProvider' => 'tmdb',
     'providers' => [
         'tmdb' => [
-            'apiKey' => 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhY2U4YmFlMDIzOGIxMGU4NjlkNGU1YjU1ODU3MTI4NyIsInN1YiI6IjY0ZmY0ZGU0MGJiMDc2MDBlMWI4YjZlOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9QOCPLxPqioe0p9_YwEtcehqLWLHKJJ9HY5Y1UNFLN4',
+            'apiKey' => $_ENV['TMDB_API_KEY'],
         ]
     ]
 ];
