d1be5b5b62fb6ece26e173bfa018d13e62f04781
Michael A <39175160+michaelthedev@users.noreply.github.com>
2023-12-04 19:26:11 +0100

    Add `MediaController`

--- app/Controllers/ApiController.php
+++ app/Controllers/ApiController.php
@@ -23,49 +23,4 @@ final class ApiController
             'message' => 'Welcome to '.$appName.' API'
         ]);
     }
-
-    /**
-     * Fetch Details about a movie
-     * @param int $id Movie Id
-     * @return void
-     */
-    public function movieDetail(int $id): void
-    {
-        response()->json([
-            'error' => false,
-            'message' => 'success',
-            'data' => MediaService::getMovieDetail($id)
-        ]);
-    }
-
-    /**
-     * Fetch details about a tv show
-     * @param int $id Tv show id
-     * @return void
-     */
-    public function tvDetail(int $id): void
-    {
-        response()->json([
-            'error' => false,
-            'message' => 'success',
-            'data' => MediaService::getTvDetail($id)
-        ]);
-    }
-
-    public function search(): void
-    {
-        validate([
-            'query' => 'required',
-            'type' => 'required|in:tv,movie,all'
-        ], input()->getOriginalParams());
-
-        $query = input()->get('query');
-        $type = input()->get('type');
-
-        response()->json([
-            'error' => false,
-            'message' => 'success',
-            'data' => MediaService::search($query, $type)
-        ]);
-    }
 }
---
+++ app/Controllers/MediaController.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+use App\Services\MediaService;
+
+/**
+ * Media Controller
+ *
+ * @package App\Controllers
+ * @author Michael Arawole <michael@logad.net>
+ */
+final class MediaController
+{
+	private MediaService $mediaService;
+
+	public function __construct()
+	{
+		$this->mediaService = new MediaService();
+	}
+
+    /**
+     * Fetch Details about a movie
+     * @param int $id Movie Id
+     * @return void
+     */
+    public function movieDetail(int $id): void
+    {
+        response()->json([
+            'error' => false,
+            'message' => 'success',
+            'data' => $this->mediaService
+				->getMovieDetail($id)
+        ]);
+    }
+
+    /**
+     * Fetch details about a tv show
+     * @param int $id Tv show id
+     * @return void
+     */
+    public function tvDetail(int $id): void
+    {
+        response()->json([
+            'error' => false,
+            'message' => 'success',
+            'data' => $this->mediaService
+				->getTvDetail($id)
+        ]);
+    }
+
+    public function search(): void
+    {
+        validate([
+            'query' => 'required',
+            'type' => 'required|in:tv,movie,all'
+        ], input()->getOriginalParams());
+
+        $query = input()->get('query');
+        $type = input()->get('type');
+
+        response()->json([
+            'error' => false,
+            'message' => 'success',
+            'data' => MediaService::search($query, $type)
+        ]);
+    }
+}
