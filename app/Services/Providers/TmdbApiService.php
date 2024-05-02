d35fd63a3a87584ec4fbb53a4167bbf2cd7a496c
Michael A. <39175160+michaelthedev@users.noreply.github.com>
2024-05-02 15:17:29 +0100

    chore: use data transformer for response

--- app/Services/Providers/TmdbApiService.php
+++ app/Services/Providers/TmdbApiService.php
@@ -256,78 +256,24 @@ final class TmdbApiService implements ApiProviderInterface
 			]
 		]);
 
-        $response = json_decode($request->getBody()->getContents(), true);
-        return new MovieDetail(
-            id: $response['id'],
-            type: 'movie',
-            title: $response['title'],
-            genres: $response['genres'],
-            rating: $response['vote_average'],
-            runtime: $response['runtime'],
-            tagline: $response['tagline'],
-            overview: $response['overview'],
-            imageUrl: $this->formatImageUrl($response['poster_path']),
-            releaseDate: $response['release_date'],
-			trailerUrl: $response['video'],
-            backdropUrl: $this->formatImageUrl($response['backdrop_path'], true),
-            releaseYear: $this->formatReleaseDate($response['release_date'])
-        );
+        $response = json_decode($request->getBody()
+			->getContents(), true);
+
+		return $this->transformer
+			->transform($response)
+			->to('movie');
     }
 
     public function getTvDetails(int $id): TvDetail
     {
 		$request = $this->client->get('tv/'.$id);
 
-        $response = json_decode($request->getBody()->getContents(), true);
+        $response = json_decode($request->getBody()
+			->getContents(), true);
 
-        // Last episode
-        $lastEpisodeDto = $this->getEpisodeDto($response['last_episode_to_air']);
-
-        // Next episode
-        $nextEpisodeDto = $this->getEpisodeDto($response['next_episode_to_air']);
-
-        return new TvDetail(
-            id: $response['id'],
-            type: 'tv',
-            title: $response['name'],
-            genres: $response['genres'],
-            rating: $response['vote_average'],
-            status: $response['status'],
-            runtime: $response['episode_run_time'][0] ?? 0,
-            seasons: $response['number_of_seasons'],
-            tagline: $response['tagline'],
-            overview: $response['overview'],
-            imageUrl: $this->formatImageUrl($response['poster_path']),
-            releaseDate: $response['first_air_date'],
-            backdropUrl: $this->formatImageUrl($response['backdrop_path'], true),
-            releaseYear: $this->formatReleaseDate($response['first_air_date']),
-            lastEpisode: $lastEpisodeDto,
-            nextEpisode: $nextEpisodeDto,
-        );
-    }
-
-    /**
-     * @param ?array $episode
-     * @return TvEpisode|null
-     */
-    private function getEpisodeDto(?array $episode): ?TvEpisode
-    {
-        if (empty($episode)) {
-            $episodeDto = null;
-        } else {
-            $episodeDto = new TvEpisode(
-                id: $episode['id'],
-                title: $episode['name'],
-                rating: $episode['vote_average'],
-                season: $episode['season_number'],
-                runtime: $episode['runtime'],
-                episode: $episode['episode_number'],
-                overview: $episode['overview'],
-                imageUrl: $this->formatImageUrl($episode['still_path']),
-                releaseDate: $episode['air_date'],
-            );
-        }
-        return $episodeDto;
+		return $this->transformer
+			->transform($response)
+			->to('tv');
     }
 
     public function search(string $query, string $type): array
